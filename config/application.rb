6bd651a56659b6af4b637bb70f7fa0f2c5442243
Pedro Cobos <pedro.cobos@live.com>
2022-09-10 19:48:05 -0300

    add rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module UboDappApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :options]
+      end
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
