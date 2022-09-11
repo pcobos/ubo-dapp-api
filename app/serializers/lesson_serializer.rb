2e2240aef4b0d2c3013cd580a5fffc335fd22fad
Pedro Cobos <pedro.cobos@live.com>
2022-09-11 07:03:01 -0300

    add name to session serializer

--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -1,4 +1,4 @@
 class LessonSerializer < ActiveModel::Serializer
-  attributes :id, :description, :video_link, :course_id
+  attributes :id, :name, :description, :video_link, :course_id
   has_many :lesson_questions
 end
